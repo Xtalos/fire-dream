f53780d547078dc4c13ea0b8f83f60ba7c5898a6
Andrea Introini <talos1986@gmail.com>
2022-02-10 00:35:21 +0100

    - fixed env error

--- util/firebase-admin.ts
+++ util/firebase-admin.ts
@@ -3,8 +3,8 @@ import { getAuth } from 'firebase-admin/auth';
 
 initializeApp({
     credential: cert({
-        privateKey: process.env.NEXT_PRIVATE_FIREBASE_ADMIN_PRIVATE_KEY,
-        clientEmail: process.env.NEXT_PRIVATE_FIREBASE_ADMIN_CLIENT_EMAIL,
+        privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY,
+        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     }),
     databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
