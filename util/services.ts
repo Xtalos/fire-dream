ac3947f49abb9b385b7e171734847e2ac9e8bff6
Andrea Introini <talos1986@gmail.com>
2022-04-14 15:01:31 +0200

    - improved updateQuotes functionality

--- util/services.ts
+++ util/services.ts
@@ -13,7 +13,7 @@ export const updateQuotes = async (assets: Asset[], config?: Config) => {
   const assetValuesCollection = collection(firestore, 'assetsValues');
 
   const quotes = response.status === 200 ? response.data.values as AssetValue[] : [];
-  quotes.filter(quote => quote.value).forEach(quote => {
+  quotes.filter(quote => quote.quantity && quote.value !== undefined).forEach(quote => {
     const assetValueRef = doc(assetValuesCollection);
     const assetRef = doc(firestore, 'assets/' + quote.assetId);
     batch.set(assetValueRef, quote);
