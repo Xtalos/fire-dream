6b9139c0af49e60b756eb752c90a277b26459739
Andrea Introini <talos1986@gmail.com>
2023-06-06 09:34:39 +0200

    - added move Asset to another owned Wallet functionality

--- components/index.ts
+++ components/index.ts
@@ -4,6 +4,7 @@ import AssetList from './asset-list';
 import AssetValue from './asset-value-form';
 import AssetsValuesFormList from './assets-values-form-list';
 import AssetsValuesFilter from './assets-values-filter';
+import AssetWalletTransferForm from './asset-wallet-transfer-form';
 
 export {
     FireDreamContainer,
@@ -11,5 +12,6 @@ export {
     AssetList,
     AssetValue,
     AssetsValuesFormList,
-    AssetsValuesFilter
+    AssetsValuesFilter,
+    AssetWalletTransferForm
 }
